49e0d5d8dca4c069d2d7613a07b64ee9b99bec2b
DEV RISHI VERMA <devrishiverma251@gmail.com>
2025-09-20 13:50:55 +0530

    added data loader and weight initilization

--- train_gpt2.py
+++ train_gpt2.py
@@ -11,6 +11,7 @@ class CausalSelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT=1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer(
@@ -41,6 +42,7 @@ class MLP(nn.Module):
         self.c_fc=nn.Linear(config.n_embd,4*config.n_embd)
         self.gelu=nn.GELU()
         self.c_proj=nn.Linear(4*config.n_embd,config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT=1
         
        
     
@@ -84,6 +86,44 @@ class GPT(nn.Module):
             "ln_f": nn.LayerNorm(config.n_embd)
         })
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        #weight sharing
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+    
+    def _init_weights(self,module):
+        for module in self.modules():
+            std=0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std*=(2*self.config.n_layer)**-0.5
+                if isinstance(module, nn.Linear):
+                    torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+                if module.bias is not None:
+                    torch.nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.Embedding):
+                torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            elif isinstance(module, nn.LayerNorm):
+                torch.nn.init.zeros_(module.bias)
+                torch.nn.init.ones_(module.weight)
+    
+    def forward(self,idx,targets=None):
+        B,T = idx.size()
+        assert T <= self.config.block_size, "Cannot forward, model block size is exhausted."
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+        x = tok_emb + pos_emb
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+        
+        logits = self.lm_head(x)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits,loss
+    
     #loading pre-trained weights from huggingface copy-pasted code
     @classmethod
     def from_pretrained(cls, model_type):   
@@ -138,6 +178,83 @@ class GPT(nn.Module):
 
 
 #########################
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+    device = 'mps' #for mac
+print(f"using device: {device}")
 
-model=GPT.from_pretrained('gpt2')
+
+num_return_sequences=1
+max_lenght=30
+
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self,B,T):
+        self.B=B
+        self.T=T
+        self.enc=tiktoken.get_encoding("gpt2")
+        with open("input.txt",'r') as f:
+            text=f.read()
+        tokens=self.enc.encode(text)
+        self.tokens=torch.tensor(tokens)
+        print(f'loaded {self.tokens} tokens')
+        print(f'1 epoch = {len(self.tokens)//(B*T)} batches')
+
+        self.current_position=0
+    def next_batch(self):
+        B,T=self.B,self.T
+        buf=self.tokens[self.current_position:self.current_position+B*T+1]
+        x=buf[:-1].view(B,T).to(device)
+        y=buf[1:].view(B,T).to(device)
+        self.current_position+=B*T
+        if self.current_position+B*T+1>=len(self.tokens):
+            self.current_position=0
+        return x,y
+
+torch.manual_seed(1337)
+if device=='cuda':
+    torch.cuda.manual_seed(1337)
+train_loader = DataLoaderLite(B=4,T=32)
+
+#model=GPT.from_pretrained('gpt2')
+
+model=GPT(GPTConfig())
+model.eval()
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+
+for i in range(50):
+    x,y=train_loader.next_batch()
+    optimizer.zero_grad()
+    logits,loss=model(x,y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i+1}, loss: {loss.item():.4f}")
+    
+    print(loss.item())
+import sys; sys.exit(0)
+
 print("didnt crash yay!")
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1)<max_lenght:
+    with torch.no_grad():
+        logits=model(x)
+        logits=logits[:,-1,:]
+        probs=F.softmax(logits,dim=-1)
+        topk_probs,topk_indices=torch.topk(probs,50,dim=-1)
+
+        ix=torch.multinomial(topk_probs,1)
+        xcol=torch.gather(topk_indices,1,ix)
+        x=torch.cat((x,xcol),dim=1)
+
+for i in range(num_return_sequences):
+    tokens=x[i,:max_lenght].tolist()
+    text=enc.decode(tokens)
+    print(f"=== SAMPLE {i+1} ===")
+    print(text)
